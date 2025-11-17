ac2c9eb69d02f981a2261056e55738ff925a8f17
SanchezFrancisco98 <fsanchezp@ucenfotec.ac.cr>
2025-11-16 18:42:43 -0600

    Este es un nuevo cambio aplicado

---
+++ mi_archivo.js
@@ -0,0 +1,15 @@
+public class HolaMundo { 
+
+    public static void main(String[] args) { 
+    saludar("nombre"); 
+    sumar(5, 3); 
+
+    } 
+    public static void saludar(String nombre) { 
+        System.out.println("Hola, " + nombre + ". ¡Bienvenida al ejercicio de Git y GitHub!"); 
+    } 
+    public static void sumar(int a, int b) { 
+        int resultado = a + b;
+        System.out.println("La suma de " + a + " + " + b + " es: " + resultado); 
+    }
+}
